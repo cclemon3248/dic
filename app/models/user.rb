351d99c0bcec0358d6ad7848a758d810222f0c8f
ジュノ <98253645+cclemon3248@users.noreply.github.com>
2022-05-02 18:15:45 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   before_validation { email.downcase! }
-  validates :password, length: { minimum: 10 }
+  validates :password, length: { minimum: 7 }
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
